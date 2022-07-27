ac2efd1fb6a648f2ec6a35b6e118a1a39ac706f6
Venkateswarlu <104353852+venkikgit@users.noreply.github.com>
2022-07-27 12:15:54 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const router= express.Router();
 const passport = require('passport');
 
 const userController = require('../controllers/user_controller');
-
+// Sign up and sign in routes
 router.get('/sign-up', userController.signup);
 router.post('/create', userController.create);
 router.get('/sign-in', userController.signIn);
@@ -13,8 +13,9 @@ router.post('/create-session',passport.authenticate(
     'local',
     {failureRedirect:'/users/sign-in'},
 ),userController.createSession);
-
+// profile
 router.get('/profile',passport.checkAuthentication, userController.profile);
+// Sign out 
 router.get('/sign-out',userController.destroySession);
 
 // google sign in routes
@@ -28,6 +29,7 @@ router.post('/forgot',userController.sendPassword);
 // update passwrod
 router.get('/update-password',passport.checkAuthentication,userController.updatePasswordForm);
 router.post('/update/:id',passport.checkAuthentication,userController.updatePassword);
+// reset password
 router.get('/reset/:id/:token', userController.resetPasswordform);
 router.post('/reset/:id/:token',userController.resetPasswordUpdated);
 module.exports = router;
